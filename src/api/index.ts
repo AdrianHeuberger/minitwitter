f49415d0c8cc105b7349c0e371432f15505845f5
simonsscherer <simon@scherer-herger.ch>
2025-02-19 23:43:03 +0100

    7.3.h

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,13 @@
 import type { Express } from 'express'
 import { initializePostsAPI } from './posts'
+import authMiddleware from './auth-middleware'
+import { initializeAuthAPI } from './auth'
 
 export const initializeAPI = (app: Express) => {
-    initializePostsAPI(app)
+      // Use the auth middleware
+  app.use(authMiddleware)
+
+  // Initialize APIs
+  initializePostsAPI(app)
+  initializeAuthAPI(app)
 }
