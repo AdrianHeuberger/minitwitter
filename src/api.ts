07382bd1fc6e00045c554201ed0ee8057e87dd71
simonsscherer <simon@scherer-herger.ch>
2025-02-19 21:50:51 +0100

    adjustment login

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
 import { type Express } from 'express'
 import { initializePostsAPI } from './api/posts'
+import { initializeAuthAPI } from './api/auth';
 
 export const initializeAPI = (app: Express) => {
   initializePostsAPI(app);
+  initializeAuthAPI(app);
 }
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import { type Express, type Request, type Response } from 'express'
 import { db } from '../database'
-import { postsTable, usersTable } from '../db/schema'
+import { usersTable } from '../db/schema'
 import { eq } from 'drizzle-orm'
 import bcrypt from 'bcrypt'
 
@@ -15,5 +15,27 @@ export const initializeAuthAPI = (app: Express) => {
             username, password: passwordHash
         }).returning()
         res.send({ id: newUser[0].id, username: newUser[0].username })
+    })
+    app.post('/api/auth/login', async (req: Request, res: Response) => {
+        const { username, password } = req.body
+
+        try {
+            const existingUsers = await db.select().from(usersTable).where(eq(usersTable.username, username))
+            if (existingUsers.length === 0) {
+                res.status(401).send({ error: 'Invalid username or password.' })
+                return
+            }
+
+            const existingUser = existingUsers[0]
+            const passwordMatch = await bcrypt.compare(password, existingUser.password)
+            if (!passwordMatch) {
+                res.status(401).send({ error: 'Invalid username or password.' })
+                return
+            }
+
+            res.send({ id: existingUser.id, username: existingUser.username })
+        } catch (error) {
+            res.status(500).send({ error: 'Error logging in user' })
+        }
     })
 }
